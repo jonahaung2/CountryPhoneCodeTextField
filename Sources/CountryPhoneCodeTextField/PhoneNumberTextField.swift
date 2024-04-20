c25e36a7f39f48734768074747f835de1a316ce3
Aung Ko Min <aung_ko_min@transitlink.com.sg>
2024-04-21 03:44:40 +0800

    hotfix

--- Sources/CountryPhoneCodeTextField/PhoneNumberTextField.swift
+++ Sources/CountryPhoneCodeTextField/PhoneNumberTextField.swift
@@ -10,22 +10,18 @@ import XUI
 
 public struct PhoneNumberTextField: View {
     
-    private var phoneNumber: Binding<PhoneNumber>
+    @Binding public var phoneNumber: PhoneNumber
     @FocusState private var isTextFieldFocused: Bool?
-    
-    public init(phoneNumber: Binding<PhoneNumber>) {
-        self.phoneNumber = phoneNumber
-    }
     
     public var body: some View {
         HStack {
-            Text(phoneNumber.wrappedValue.countryCode.flag)
-                ._presentSheet {
-                    CountryCodePickerView(countryCode: phoneNumber.countryCode)
+            Text(phoneNumber.countryCode.flag)
+                ._tapToPush {
+                    CountryCodePickerView(countryCode: $phoneNumber.countryCode)
                 }
             Divider()
             
-            TextField("\(phoneNumber.wrappedValue.countryCode.country) mobile no", text: phoneNumber.number)
+            TextField("\(phoneNumber.countryCode.country) mobile no", text: $phoneNumber.number)
                 .textContentType(.telephoneNumber)
                 .keyboardType(.phonePad)
                 .focused($isTextFieldFocused, equals: true)
